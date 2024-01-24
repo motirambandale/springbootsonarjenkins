94424e7c9bff896af765055aaaed707258760d5d
javabyram <bandalemp0208@gmail.com>
2024-01-25 00:18:04 +0530

    first comiit

---
+++ src/main/java/com/mit/springbootsonarjenkins/SpringbootsonarjenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.mit.springbootsonarjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsonarjenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsonarjenkinsApplication.class, args);
+	}
+
+}
